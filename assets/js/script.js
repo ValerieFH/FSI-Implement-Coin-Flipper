563aacc9e0b13cf01249b9b08666b2e4dcddb810
Valerie Haupt <valre@valeries-mbp-2.attlocal.net>
2022-02-18 15:44:15 -0600

    refactored JS into functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,60 +1,55 @@
 let headCount = 0
 let tailCount = 0
 
-//TODO: refactor to use functions
+function updateCounts (){
+    document.querySelector('#heads').textContent = headCount
+    document.querySelector('#tails').textContent = tailCount
+}
+
+function updatePercents (){
+    if (headCount + tailCount == 0){
+        document.querySelector('#heads-percent').textContent = '0%'
+        document.querySelector('#tails-percent').textContent = '0%'
+    } else {
+        document.querySelector('#heads-percent').textContent = Math.round(headCount/(headCount+tailCount) * 100) + '%'
+        document.querySelector('#tails-percent').textContent = Math.round(tailCount/(headCount+tailCount) * 100) + '%'
+    }
+}
+
+function changeMessage(text){
+    document.querySelector('#message').textContent = text
+}
+
+function changePicture(src, alt) {
+    let pennyFace = document.querySelector('#penny')
+    pennyFace.src = src
+    pennyFace.alt = alt
+}
 
 document.addEventListener('DOMContentLoaded', function () {
 
     document.querySelector('#flip').addEventListener('click', function(e){
         if (Math.random() > 0.5) {
-            console.log('heads')
-            let pennyFace = document.querySelector('#penny')
-            pennyFace.src = "assets/images/penny-heads.jpg"
-            pennyFace.alt = "2005 Penny, head side"
-
-            document.querySelector('#message').textContent = "You Flipped Heads!"
-
+            changePicture("assets/images/penny-heads.jpg","2005 Penny, head side")
+            changeMessage("You Flipped Heads!")
             headCount++
 
-            document.querySelector('#heads').textContent = headCount
-            document.querySelector('#heads-percent').textContent = Math.round(headCount/(headCount+tailCount) * 100) + '%'
-            document.querySelector('#tails-percent').textContent = Math.round(tailCount/(headCount+tailCount) * 100) + '%'
-                } else {
-            console.log('tails')
-            let pennyFace = document.querySelector('#penny')
-            pennyFace.src = "assets/images/penny-tails.jpg"
-            pennyFace.alt = "2005 Penny, head side"
+            } else {
 
-            document.querySelector('#message').textContent = "You Flipped Tails!"
-
+            changePicture("assets/images/penny-tails.jpg","2005 Penny, head side")
+            changeMessage("You Flipped Tails!")
             tailCount++
-
-            document.querySelector('#tails').textContent = tailCount
-            document.querySelector('#heads-percent').textContent = Math.round(headCount/(headCount+tailCount) * 100) + '%'
-            document.querySelector('#tails-percent').textContent = Math.round(tailCount/(headCount+tailCount) * 100) + '%'
         }
-        // Flip Button Click Handler
-            // TODO: Update image and status message in the DOM
-    
-            // Update the scorboard
-            // TODO: Calculate the total number of rolls/flips
-            // Make variables to track the percentages of heads and tails
-            // TODO: Use the calculated total to calculate the percentages
-            // HINT: Make sure not to divide by 0! (if total is 0, percent will be 0 as well)
-            // TODO: Update the display of each table cell
+        updateCounts()
+        updatePercents()
     })
 
     document.querySelector('#clear').addEventListener('click', function(e){
-        // Clear Button Click Handler
-            // TODO: Reset global variables to 0
             headCount = 0
             tailCount = 0
-            document.querySelector('#message').textContent = "Play Again!"
-            // TODO: Update the scoreboard (same logic as in flip button click handler)
-            document.querySelector('#heads').textContent = headCount
-            document.querySelector('#tails').textContent = tailCount
-            document.querySelector('#heads-percent').textContent = '0%'
-            document.querySelector('#tails-percent').textContent = '0%'
+            changeMessage("Play Again!")
+            updateCounts()
+            updatePercents()
     })
 
 })
